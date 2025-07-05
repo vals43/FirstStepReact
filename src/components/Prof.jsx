e43043ffa32c963c239507886c7ac740becfb8c8
Dr Herilala <hei.teddy.3@gmail.com>
2025-07-05 08:49:53 +0300

    add : image in slide

--- src/components/Prof.jsx
+++ src/components/Prof.jsx
@@ -7,7 +7,9 @@ import 'swiper/css/navigation';
 import 'swiper/css/autoplay';
 import 'swiper/css/effect-fade';
 import Slide from './Slide';
-import logoHei from '../assets/img/Logo HEI.png';
+import banner1 from '../assets/img/hei-banner-1.webp';
+import banner2 from '../assets/img/hei-banner-2.webp';
+import banner3 from '../assets/img/hei-banner-3.webp';
 
 function Prof() {
   return (
@@ -28,13 +30,13 @@ function Prof() {
       className='text-black'
     >
       <SwiperSlide>
-        <Slide texte="Des entreprises partenaires et une équipe pédagogique et administrative qui travaille sans relâche pour la montée en compétences de nos étudiants." backgroundImg={logoHei} />
+        <Slide texte="Des entreprises partenaires et une équipe pédagogique et administrative qui travaille sans relâche pour la montée en compétences de nos étudiants." backgroundImg={banner1} />
       </SwiperSlide>
       <SwiperSlide>
-      <Slide texte="« L’éducation est l’arme la plus puissante pour changer le monde » selon Nelson Mandela. « L’éducation dans l’informatique est une arme très puissante pour lutter contre la pauvreté à Madagascar » selon HEI. C’est notre mission." backgroundImg={logoHei} />
+      <Slide texte="« L’éducation est l’arme la plus puissante pour changer le monde » selon Nelson Mandela. « L’éducation dans l’informatique est une arme très puissante pour lutter contre la pauvreté à Madagascar » selon HEI. C’est notre mission." backgroundImg={banner2} />
       </SwiperSlide>
       <SwiperSlide>
-      <Slide texte="Une infrastructure de qualité et un programme pédagogique qui répond au besoin du marché pour l’employabilité de nos étudiants." backgroundImg={logoHei} />
+      <Slide texte="Une infrastructure de qualité et un programme pédagogique qui répond au besoin du marché pour l’employabilité de nos étudiants." backgroundImg={banner3} />
       </SwiperSlide>
     </Swiper>
   );
--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -5,8 +5,8 @@ function Slide({ texte, backgroundImg }) {
         <div 
             style={{ backgroundImage: `url(${backgroundImg})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
             className="w-full h-screen flex flex-col justify-center items-center text-center text-white"        >
-            <h1 className="text-5xl font-bold mb-4 text-blue-950">Haute Ecole d'Informatique</h1>
-            <p className="text-xl mb-8 max-w-2xl mx-auto text-blue-950">{texte}</p>
+            <h1 className="text-5xl font-bold mb-4 text-yellow-600">Haute Ecole d'Informatique</h1>
+            <p className="text-xl mb-8 max-w-2xl mx-auto text-white">{texte}</p>
             <div className="flex gap-4">
                 <button className='px-4 py-2 text-sm text-blue-950 duration-100 bg-white rounded-md shadow-md focus:shadow-none ring-offset-2 ring-blue-600 focus:ring-2'>
                     Inscriver vous ici
